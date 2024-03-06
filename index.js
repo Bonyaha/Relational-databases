62aefffad41a59f7c7dcc706754f9adc87a2c3d9
Roman <romchikus1987@gmail.com>
2024-03-06 20:18:02 +0200

    updated

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const express = require('express')
+const app = express()
+app.use(express.json())
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+	/* dialectOptions: {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false
+		}
+	}, */
+})
 
-const main = async () => {
+class Note extends Model { }
+Note.init({
+	id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+	content: { type: DataTypes.TEXT, allowNull: false },
+	important: { type: DataTypes.BOOLEAN },
+	date: { type: DataTypes.DATE }
+}, {
+	sequelize,
+	underscored: true,
+	timestamps: false,
+	modelName: 'note'
+})
+
+app.get('/api/notes', async (req, res) => {
+	const notes = await Note.findAll()
+	res.json(notes)
+})
+
+app.post('/api/notes', async (req, res) => {
 	try {
-		await sequelize.authenticate()
-		const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
-		console.log(notes)
-		sequelize.close()
+		const note = await Note.create(req.body)
+		return res.json(note)
 	} catch (error) {
-		console.error('Unable to connect to the database:', error)
+		return res.status(400).json({ error })
 	}
-}
+})
 
-main()
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
